131381850e617f565d350fae49cc0ebcbec20c58
jconejero <juan.conejero@pixinsight.com>
2017-01-25 21:07:18 +0100

    XISF Version 1.0 Specification - DRAFT 9.1

--- pidoc/XISF-1.0-spec/ByteShuffling.cpp
+++ pidoc/XISF-1.0-spec/ByteShuffling.cpp
@@ -1,47 +1,64 @@
 #include <cstdint>
+#include <string.h>
 
 /*!
- * Byte shuffling algorithm applied to size bytes starting at input, with
- * element length itemSize in bytes. Writes the shuffled data to the output
- * array.
+ * Byte shuffling algorithm.
+ *
+ * output      The output array, where the shuffled array will be stored. This
+ *             array must have capability for at least size bytes.
+ *
+ * input       The source unshuffled array.
+ *
+ * size        Length in bytes of the arrays.
+ *
+ * itemSize    Length of an array item.
  */
 void Shuffle( uint8_t* output, const uint8_t* input, size_t size, size_t itemSize )
 {
    if ( size > 0 )
-      if ( input != nullptr )
-         if ( output != nullptr )
-         {
-            size_t numberOfItems = size / itemSize;
-            uint8_t* s = output;
-            for ( size_t j = 0; j < itemSize; ++j )
+      if ( itemSize > 0 )
+         if ( input != nullptr )
+            if ( output != nullptr )
             {
-               const uint8_t* u = input + j;
-               for ( size_t i = 0; i < numberOfItems; ++i, ++s, u += itemSize )
-                  *s = *u;
+               size_t numberOfItems = size / itemSize;
+               uint8_t* s = output;
+               for ( size_t j = 0; j < itemSize; ++j )
+               {
+                  const uint8_t* u = input + j;
+                  for ( size_t i = 0; i < numberOfItems; ++i, ++s, u += itemSize )
+                     *s = *u;
+               }
+               ::memcpy( s, input + numberOfItems*itemSize, size % itemSize );
             }
-            ::memcpy( s, input + numberOfItems*itemSize, size % itemSize );
-         }
 }
 
 /*!
- * Reverse byte shuffling algorithm applied to size bytes starting at input,
- * with element length itemSize in bytes. Writes the unshuffled data to the
- * output array.
+ * Reverse byte shuffling algorithm.
+ *
+ * output      The output array, where the unshuffled array will be stored.
+ *             This array must have capability for at least size bytes.
+ *
+ * input       The source shuffled array.
+ *
+ * size        Length in bytes of the arrays.
+ *
+ * itemSize    Length of an array item.
  */
 void Unshuffle( uint8_t* output, const uint8_t* input, size_t size, size_t itemSize )
 {
    if ( size > 0 )
-      if ( input != nullptr )
-         if ( output != nullptr )
-         {
-            size_t numberOfItems = size / itemSize;
-            const uint8_t* s = input;
-            for ( size_t j = 0; j < itemSize; ++j )
+      if ( itemSize > 0 )
+         if ( input != nullptr )
+            if ( output != nullptr )
             {
-               uint8_t* u = output + j;
-               for ( size_t i = 0; i < numberOfItems; ++i, ++s, u += itemSize )
-                  *u = *s;
+               size_t numberOfItems = size / itemSize;
+               const uint8_t* s = input;
+               for ( size_t j = 0; j < itemSize; ++j )
+               {
+                  uint8_t* u = output + j;
+                  for ( size_t i = 0; i < numberOfItems; ++i, ++s, u += itemSize )
+                     *u = *s;
+               }
+               ::memcpy( output + numberOfItems*itemSize, s, size % itemSize );
             }
-            ::memcpy( output + numberOfItems*itemSize, s, size % itemSize );
-         }
 }
